b429d402c058716b10fb727d4bc1c3c607331da5
Leffin <leffin.christopher@gmail.com>
2025-09-15 16:01:59 -0400

    Updated spped of autoscroll

--- src/components/hooks.ts
+++ src/components/hooks.ts
@@ -36,8 +36,8 @@ export function useScrollYwithAutoScroll(): number {
         return;
       }
 
-      const delta = (targetY - currentY) * 0.01;
-      const step = Math.sign(delta) * Math.min(Math.abs(delta), 3);
+      const delta = (targetY - currentY) * 0.02;
+      const step = Math.sign(delta) * Math.min(Math.abs(delta), 9);
       const nextY = currentY + step;
 
       window.scrollTo(0, nextY);
