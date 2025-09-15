2fa9271c67d43af2df1c5781529232220d6e079c
Leffin <leffin.christopher@gmail.com>
2025-09-15 19:21:40 -0400

    updated og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,17 +20,17 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://use.typekit.net/qje5ynx.css" />
         {/* <link id="favicon" rel="icon" href="/favicon-light.ico" /> */}
 
-        <meta property="og:title" content="Leffin - Artist Portfolio" />
-        <meta property="og:description" content="Leffin and his interactive experiential art projects" />
-        <meta property="og:image" content="%PUBLIC_URL%/leffin_portfolio.png" />
+        <meta property="og:title" content="Leffin | An Interactive & Experiential Artist" />
+        <meta property="og:description" content="Leffin is an artist who blends technology and creativity. Explore his immersive projects here." />
+        <meta property="og:image" content="https://leff.in/leffin_portfolio.png" />
         <meta property="og:url" content="https://leff.in" />
         <meta property="og:type" content="website" />
 
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Leffin - Artist Portfolio" />
-        <meta name="twitter:description" content="Leffin and his interactive experiential art projects" />
-        <meta name="twitter:image" content="%PUBLIC_URL%/leffin_portfolio.png" />
+        <meta name="twitter:title" content="Leffin | An Interactive & Experiential Artist" />
+        <meta name="twitter:description" content="Leffin is an artist who blends technology and creativity. Explore his immersive projects here." />
+        <meta name="twitter:image" content="https://leff.in/leffin_portfolio.png" />
 
 
       </head>
