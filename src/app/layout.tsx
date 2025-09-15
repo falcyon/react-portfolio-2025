574223670e574d4a87f1fb5b8fea595fd1fc70af
Leffin <leffin.christopher@gmail.com>
2025-09-15 19:02:31 -0400

    Added Open Graph Tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 import GridOverlay from "@/components/GridOverlay";
 
+
 export const metadata: Metadata = {
   title: "Leffin - Portfolio",
-  description: "Leffin's personal portfolio showcasing projects, skills, and experiences.",
+  description: "Leffin's personal art portfolio showcasing projects, skills, and experiences.",
 };
 
 
@@ -17,6 +18,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/qje5ynx.css" />
+        {/* <link id="favicon" rel="icon" href="/favicon-light.ico" /> */}
+
+        <meta property="og:title" content="Leffin - Artist Portfolio" />
+        <meta property="og:description" content="Leffin and his interactive experiential art projects" />
+        <meta property="og:image" content="%PUBLIC_URL%/Leffin_Portfolio.png" />
+        <meta property="og:url" content="https://leff.in" />
+        <meta property="og:type" content="website" />
+
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Leffin - Artist Portfolio" />
+        <meta name="twitter:description" content="Leffin and his interactive experiential art projects" />
+        <meta name="twitter:image" content="%PUBLIC_URL%/Leffin_Portfolio.png" />
+
 
       </head>
       <body>
