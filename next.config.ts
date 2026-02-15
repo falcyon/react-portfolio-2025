651a3b0a93ad58f7c19fe66fbedb46dbc0e6c92e
Leffin <leffin7@gmail.com>
2026-02-15 16:57:56 -0500

    moved back to vercel.json

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/dinoRevenge/:path*",
-        destination: "/interactives/dinoRevenge/:path*",
-        permanent: false,
-      },
-      {
-        source: "/dinoRevenge",
-        destination: "/interactives/dinoRevenge",
-        permanent: false,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/interactives/dinoRevenge/:path*",
-        destination: "https://gemini-doodle.vercel.app/:path*",
-      },
-      {
-        source: "/interactives/dinoRevenge",
-        destination: "https://gemini-doodle.vercel.app/",
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
